f478df53294c602223ac8c23207d7ee4fab1804e
lalitajadhav <lalitajadhav10@gmail.com>
2021-05-13 23:36:51 +0100

    spring cloud config

--- src/main/java/com/example/CreditCardApplication/CreditCardApplication.java
+++ src/main/java/com/example/CreditCardApplication/CreditCardApplication.java
@@ -3,7 +3,10 @@ package com.example.CreditCardApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -18,4 +21,16 @@ public class CreditCardApplication {
         SpringApplication.run(CreditCardApplication.class, args);
     }
 
+    @Configuration
+    public static class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                    .oauth2Login();
+        }
+    }
 }
